0c3b80254e4f98884f1929b44638023b6df47f8d
Dilan Jimenez Arroyave <dilanjimenez140@gmail.com>
2022-10-01 23:49:34 -0500

    MySQL

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import {pool} from "./db.js";
 
 const app = express();
+
+app.get('/ping', async (req, res) => {
+  const [result] = await pool.query('SELECT "pong" AS result;');
+  res.json(result[0]);
+});
 
 app.listen(3000);
 console.log('Server running on port 3000');
