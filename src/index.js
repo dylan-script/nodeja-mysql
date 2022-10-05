1d8908772731fb3469f3a3f70456b7f4c9ec5694
Dilan Jimenez Arroyave <dilanjimenez140@gmail.com>
2022-10-04 23:38:45 -0500

    GET employees

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(express.json())
 
-app.use(employeesRoutes);
 app.use(indexRoutes)
+app.use('/api', employeesRoutes);
+
 
 app.listen(3000);
 console.log('Server running on port 3000');
