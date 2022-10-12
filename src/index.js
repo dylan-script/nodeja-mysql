c26c4391986e46f88129eed68fe36afe90a54b3f
Dilan Jimenez Arroyave <dilanjimenez140@gmail.com>
2022-10-11 21:57:43 -0500

    Variables de Sesión

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-import express from "express";
-import employeesRoutes from "./routes/employees.routes.js";
-import indexRoutes from "./routes/index.routes.js";
-const app = express();
+import {PORT} from './config.js'
+import {app} from './app.js'
 
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use('/api', employeesRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message : 'Endpoint Not Found'
-  })
-})
-
-app.listen(3000);
-console.log('Server running on port 3000');
+app.listen(PORT);
+console.log('Server running on port', PORT);
 
